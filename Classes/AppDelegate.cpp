c8a298c2306e164ebb1040803d447a26ef6b5bb8
rudy-alegrium <rudytan@Rudys-MacBook-Pro.local>
2016-07-13 11:38:23 +0700

    Master

---
+++ Classes/AppDelegate.cpp
@@ -0,0 +1,43 @@
+#include "AppDelegate.h"
+#include "GraphicsScene.h"
+
+USING_NS_CC;
+
+AppDelegate::AppDelegate() {
+    
+}
+
+AppDelegate::~AppDelegate()
+{
+}
+
+// if you want a different context, modify the value of glContextAttrs
+// it will affect all platforms
+void AppDelegate::initGLContextAttrs()
+{
+    // set OpenGL context attributes: red,green,blue,alpha,depth,stencil
+    GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8};
+    
+    GLView::setGLContextAttrs(glContextAttrs);
+}
+
+bool AppDelegate::applicationDidFinishLaunching() {
+    auto director = Director::getInstance();
+    auto glview = director->getOpenGLView();
+    if(!glview) {
+        glview = GLViewImpl::create("Hello World");
+        glview->setFrameSize(640, 480);
+        director->setOpenGLView(glview);
+    }
+    
+    auto scene = GraphicsScene::createScene();
+    director->runWithScene(scene);
+    
+    return true;
+}
+
+void AppDelegate::applicationDidEnterBackground() {
+}
+
+void AppDelegate::applicationWillEnterForeground() {
+}
